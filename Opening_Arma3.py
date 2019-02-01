ea931950c93e23655d6c628ae714028b67b525d4
Derek <hahnf1@udayton.edu>
2019-02-01 09:01:12 -0500

    Initial Commit

---
+++ Opening_Arma3.py
@@ -0,0 +1,6 @@
+# How to Open Arma 3 in Python
+# make sure that the path here is identical to your Arma execuatble on your PC
+
+import subprocess
+
+subprocess.Popen(['C:\Program Files (x86)\Steam\steamapps\common\Arma 3\Arma3.exe'])

c568bde82897c259d307038a36600219b99ec4f4
reynoldsp709 <reynoldsp709@gmail.com>
2019-02-01 10:17:27 -0500

    first lines of position script

---
+++ Basic_Script_Writing.py
@@ -0,0 +1,8 @@
+in1 = input('input 1: ')
+in2 = input('input 2: ')
+in3 = input('input 3: ')
+in4 = input('input 4: ')
+pos = 3
+
+print("pos%d = player modelToWorld [%s,%s,%s];" % (pos, in1, in2, in3))
+print("cam camSetPos pos%d;" % pos)

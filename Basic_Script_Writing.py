a6e8d425b6bfbb46f995d0167464b0ae7fda67ce
John Allen <allenj15@udayton.edu>
2019-02-11 08:20:03 -0500

    added angle input

--- Basic_Script_Writing.py
+++ Basic_Script_Writing.py
@@ -1,4 +1,4 @@
-def script(pos, cc):
+def script(pos, cc, angle):
     print('pos1 = player modelToWorld [0,5,5]')
     print('cam = "camera" camCreate pos1;')
     print('cam cameraEffect ["INTERNAL", "BACK"];\n')
@@ -7,7 +7,7 @@ def script(pos, cc):
     print('cam camSetDir (pos2 vectorFromTo pos1);')
     print('cam camCommit %d;\n' % cc)
     print('angleface = 0;')
-    print('angle = 10;')
+    print('angle = %d;\n' % angle)
     print('0 = [] spawn')
     print('{')
     print('\twhile {angle <= 360} do')
@@ -21,7 +21,7 @@ def script(pos, cc):
         print('\t\tcam camCommit %d;' % cc)
         print('\t\twaitUntil {camCommitted cam};')
         print('\t\tscreenshot "";\n')
-    print('\t\tunit1 setDir angle;')
+    print('\t\tunit1 setDir angleface;')
     print('\t\tangleface = angleface+angle;')
     print('\t};')
     print('};')

141f16169d2f524a0174b00967baff2458ca171b
Connor Loftus <loftusc2@udayton.edu>
2019-02-15 09:01:26 -0500

    Updated position_generator function to actually return the position array. Generating the script is then performed by calling Basic_Script_Writing with the returned array from another function. This basically just allows for the position array to be used outside the function if necessary.

--- main_script_generator.py
+++ main_script_generator.py
@@ -6,15 +6,21 @@ import sys
 def position_generator(angle_step, x_init, rota_angle_step):
     # maybe do error/type checking on the inputs later...
     curr_angle = 0
-    default_cc = 2  # can take this as an argument if we want
+    # default_cc = 2  # can take this as an argument if we want ||| removed for now
     pos_array = []
     while curr_angle < 360:
         pos_array.append(Coordinate_Generator.coords(x_init, curr_angle, 100))
         curr_angle += angle_step
+    return pos_array
+
+
+def generate_script(pos_array, rota_angle_step):
+    default_cc = 2
     Basic_Script_Writing.script(pos_array, default_cc, rota_angle_step)
 
 
 if __name__ == "__main__":
     #  probably do not need this assert?
     assert len(sys.argv) > 3, "Angle step, x-distance from center, and platform rotation angle step required as inputs"
-    position_generator(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
+    position_array = position_generator(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
+    generate_script(position_array, int(sys.argv[3]))

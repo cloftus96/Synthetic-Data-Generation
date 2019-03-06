f323c95dca732fc52fad78cf99634e650f4f5a00
Connor Loftus <loftusc2@udayton.edu>
2019-03-06 09:50:40 -0500

    added mission folder creation code

--- Basic_Script_Writing.py
+++ Basic_Script_Writing.py
@@ -11,6 +11,9 @@
 #
 #*******************************************************************************************************
 
+import os
+from pathlib import Path
+
 def arma3_script_generator(pos, cc, angle):
 
     try:
@@ -65,3 +68,44 @@ def arma3_script_generator(pos, cc, angle):
     f.write('\t};\n')
     f.write('};\n')
     f.close()
+
+def create_mission_dir():
+    """ This function creates the directory and files necessary to run our script as a mission. This includes putting
+    a new folder in the default Arma 3 missions folder, as well as providing a default mission file and an init file
+    that points to our script (this causes our script to run immediately upon playing the mission."""
+    missionsFolder = Path(os.path.expanduser('~\\Documents\\Arma 3\\missions'))
+    customMission = Path(missionsFolder, '\\DataGeneration.Altis')
+    if not customMission.exists():
+        os.mkdir(customMission, 0o777)
+    initfilepath = Path(customMission, '\\init.sqf')
+    missionfilepath = Path(customMission, '\\mission.sqm')
+
+    # write the init.sqf file
+    initfile = open(initfilepath, "w+")
+    initfile.write('execVM "Data_Generator.sqf";')
+    initfile.close()
+
+    # write the mission.sqm file
+    missionfile = open(missionfilepath, "w+")
+    missionfile.write('version=53')
+    missionfile.write('class EditorData')
+    missionfile.write('{')
+    missionfile.write('\tmoveGridStep=1;')
+    missionfile.write('\tangleGridStep=0.2617994;')
+    missionfile.write('\tscaleGridStep=1;')
+    missionfile.write('\tautoGroupingDist=10;')
+    missionfile.write('\ttoggles=1;')
+    missionfile.write('\tclass Camera')
+    missionfile.write('\t{')
+    missionfile.write('\t\tpos[]={14305.279,24.621294,17587.59};')
+    missionfile.write('\t\tdir[]={-0.31484151,-0.17364819,-0.93312436};')
+    missionfile.write('\t\tup[]={-0.055515055,0.98480767,-0.164535};')
+    missionfile.write('\t\taside[]={-0.94751924,0,0.31969842};')
+    missionfile.write('\t};')
+    missionfile.write('};')
+    missionfile.write('binarizationWanted=0;')
+    missionfile.write('class AddonsMetaData')
+    missionfile.write('{\n};')
+    missionfile.write('randomSeed=8484358;')
+    missionfile.write('class ScenarioData')
+    missionfile.write('{\n\tauthor="";\n};')

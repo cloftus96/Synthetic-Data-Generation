e00794e174f6183483b925f7cfce6c4ca93a7a9c
Derek <hahnf1@udayton.edu>
2019-03-06 09:43:23 -0500

    added error handling to open

--- Basic_Script_Writing.py
+++ Basic_Script_Writing.py
@@ -7,12 +7,19 @@
 #
 #     parameters: pos - an array of coordinates for the camera to move to
 #                 cc - time in between each screenshot
-#                 angle - the angle between each screenshot
+#                 angle - the angle between each screenshot (angle x number of loop iterations = 360)
 #
 #*******************************************************************************************************
 
 def arma3_script_generator(pos, cc, angle):
-    f = open('C:\\Program Files (x86)\\Steam\\steamapps\\common\\Arma 3\\Data_Generator.sqf', "w+")
+
+    try:
+        f = open('C:\\Program Files (x86)\\Steam\\steamapps\\common\\Arma 3\\Data_Generator.sqf', "w+")
+    except Exception:
+        print('ERROR:')
+        print('Issue opening the Data_Generator.sqf file. See arma3_script_generator function.')
+        exit(3)
+
     f.write('0 = [] spawn\n')
     f.write('{\n')
     f.write('_currentVehicle = "armaNameofVehicle" createVehicle [mapPositionCoordX, mapPositionCoordY, 0];\n')

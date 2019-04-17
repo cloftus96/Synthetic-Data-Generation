11f8338d3c345dde74aac8b1af9c9bfb3f944d7c
Connor Loftus <loftusc2@udayton.edu>
2019-04-17 09:43:59 -0400

    Trying to identify issues with index out of bounds exceptions.

--- Arma_3_PyAutoGUI.py
+++ Arma_3_PyAutoGUI.py
@@ -54,6 +54,7 @@ def main():
     # open the game and sleep to wait for the game to open
     # before running, change this path to the Arma3_64.exe file on your own computer
     try:
+        # subprocess.Popen(['D:\\Downloads for D drive\\Steam\\steamapps\\common\\Arma 3\\Arma3_x64.exe'])
         subprocess.Popen(['C:\\Program Files (x86)\\Steam\\steamapps\\common\\Arma 3\\Arma3_x64.exe'])
     except FileNotFoundError:
         print('ERROR:')

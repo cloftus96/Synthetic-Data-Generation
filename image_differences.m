5453d8964411f6a1b4771303f8e55ff2f6b80eca
John Allen <allenj15@udayton.edu>
2019-04-26 07:30:07 -0400

    Added reading and writing to csv files

--- image_differences.m
+++ image_differences.m
@@ -1,10 +1,14 @@
-clear
+clear all
 clc
+close all
+n = 1;
 
+while n < 1000000
 % difference between images
-
-a = imread('im2.jpg');
-b = imread('im3.jpg');
+first = sprintf('%d_blank.png', n);
+second = sprintf('%d.png', n);
+a = imread(first);
+b = imread(second);
 
 obj = imabsdiff(a,b);
 figure(1)
@@ -16,7 +20,7 @@ BW = imread('comp.jpg');
 I = rgb2gray(BW);
 figure(2)
 imshow(I);
-YourArray = imbinarize(I,.3);
+YourArray = imbinarize(I,.05);
 figure(3)
 imshow(YourArray);
 cc = bwlabel(YourArray,8);
@@ -24,26 +28,51 @@ figure(4)
 imagesc(cc);
 figure(5)
 hist(cc(:),[1:max(cc(:))]);
-y = cc==240;
+m = mode(cc,'all');
+cc(cc==m) = NaN;
+m = mode(cc,'all');
+cc(cc==m) = NaN;
+m = mode(cc,'all');
+y = cc==m;
+z = cc;
+figure(6)
+imagesc(cc==m);
 [row,col]= find(y);
 k = convhull(row,col);
-figure(6)
+figure(7)
 plot(row(k),col(k),'r-',row,col,'b*')
+value =[row(k) col(k)];
+minimums = min(value);
+maximums = max(value);
+maximums = maximums + 10;
+minimums = minimums - 10;
+plot(row(k),col(k),'r-',row,col,'b*',maximums(1,1),maximums(1,2),'k*',minimums(1,1),minimums(1,2),'k*')
 
-
-
-% region_minsize = 5;   %for example
-% uvals = unique(YourArray(:));
-% accumulated_list = struct('label', {}, 'props', {});
-% for K = 1 : length(uvals)
-%    U = uvals(K);
-%    accumulated_list(K).label = U;
-%    BW = YourArray == U;
-%    BW2 = bwareafilt(BW, [region_minsize, inf]);  %discard areas that are too small
-%     theseprops = regionprops(BW2, 'PixelList', 'Orientation');
-%     accumulated_list(K).props = theseprops;
-% end
-%  
-% objectofinterest = theseprops(29);
-% pixellist = objectofinterest.PixelList;
-% plot(pixellist(:,1),pixellist(:,2))
+y(minimums(1,1):maximums(1,1),maximums(1,2)) = 1;
+y(minimums(1,1):maximums(1,1),minimums(1,2)) = 1;
+y(minimums(1,1),minimums(1,2):maximums(1,2)) = 1;
+y(maximums(1,1),minimums(1,2):maximums(1,2)) = 1;
+figure(8)
+imshow(y);
+figure(9)
+z(minimums(1,1):maximums(1,1),maximums(1,2)) = 1;
+z(minimums(1,1):maximums(1,1),minimums(1,2)) = 1;
+z(minimums(1,1),minimums(1,2):maximums(1,2)) = 1;
+z(maximums(1,1),minimums(1,2):maximums(1,2)) = 1;
+imshow(z);
+[d1,d2, existingData] = xlsread('AnnotationData.csv');
+BR = sprintf('%d %d',maximums(1,1),maximums(1,2));
+TR = sprintf('%d %d',minimums(1,1),minimums(1,2));
+TL = sprintf('%d %d',maximums(1,1),minimums(1,2));
+BL = sprintf('%d %d',minimums(1,1),maximums(1,2));
+BR = cellstr(BR);
+TR = cellstr(TR);
+TL = cellstr(TL);
+BL = cellstr(BL);
+existingData(n+1,11) = (BR);
+existingData(n+1,12) = (BR);
+existingData(n+1,13) = (TL);
+existingData(n+1,14) = (BL);
+xlswrite('AnnotationData.csv',existingData)
+n = n + 1;
+end

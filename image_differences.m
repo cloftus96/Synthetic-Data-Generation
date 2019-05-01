efa10712237e3d9c30b5a4b54b3f0e6c41434700
John Allen <allenj15@udayton.edu>
2019-05-01 07:54:06 -0400

    Turned off showing images

--- image_differences.m
+++ image_differences.m
@@ -12,22 +12,22 @@ b = imread(second);
 
 obj = imabsdiff(a,b);
 figure(1)
-imshow(obj,[])
+%imshow(obj,[])
 saveas(gcf,'comp.jpg')
 
 
 BW = imread('comp.jpg'); 
 I = rgb2gray(BW);
 figure(2)
-imshow(I);
+%imshow(I);
 YourArray = imbinarize(I,.05);
 figure(3)
-imshow(YourArray);
+%imshow(YourArray);
 cc = bwlabel(YourArray,8);
 figure(4)
-imagesc(cc);
+%imagesc(cc);
 figure(5)
-hist(cc(:),[1:max(cc(:))]);
+%hist(cc(:),[1:max(cc(:))]);
 m = mode(cc,'all');
 cc(cc==m) = NaN;
 m = mode(cc,'all');
@@ -36,30 +36,30 @@ m = mode(cc,'all');
 y = cc==m;
 z = cc;
 figure(6)
-imagesc(cc==m);
+%imagesc(cc==m);
 [row,col]= find(y);
 k = convhull(row,col);
 figure(7)
-plot(row(k),col(k),'r-',row,col,'b*')
+%plot(row(k),col(k),'r-',row,col,'b*')
 value =[row(k) col(k)];
 minimums = min(value);
 maximums = max(value);
 maximums = maximums + 10;
 minimums = minimums - 10;
-plot(row(k),col(k),'r-',row,col,'b*',maximums(1,1),maximums(1,2),'k*',minimums(1,1),minimums(1,2),'k*')
+%plot(row(k),col(k),'r-',row,col,'b*',maximums(1,1),maximums(1,2),'k*',minimums(1,1),minimums(1,2),'k*')
 
 y(minimums(1,1):maximums(1,1),maximums(1,2)) = 1;
 y(minimums(1,1):maximums(1,1),minimums(1,2)) = 1;
 y(minimums(1,1),minimums(1,2):maximums(1,2)) = 1;
 y(maximums(1,1),minimums(1,2):maximums(1,2)) = 1;
 figure(8)
-imshow(y);
+%imshow(y);
 figure(9)
 z(minimums(1,1):maximums(1,1),maximums(1,2)) = 1;
 z(minimums(1,1):maximums(1,1),minimums(1,2)) = 1;
 z(minimums(1,1),minimums(1,2):maximums(1,2)) = 1;
 z(maximums(1,1),minimums(1,2):maximums(1,2)) = 1;
-imshow(z);
+%imshow(z);
 [d1,d2, existingData] = xlsread('AnnotationData.csv');
 BR = sprintf('%d %d',maximums(1,1),maximums(1,2));
 TR = sprintf('%d %d',minimums(1,1),minimums(1,2));

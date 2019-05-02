cbe1f7d04f221b4383ef2ecb35f801ff4cbca740
cloftus96 <loftusc2@udayton.edu>
2019-05-01 20:23:36 -0400

    Added functionality for adjusting the z-axis value based on user input.

--- main_script_generator.py
+++ main_script_generator.py
@@ -3,13 +3,13 @@ import Basic_Script_Writing
 import sys
 
 
-def position_generator(angle_step, x_init):
+def position_generator(angle_step, x_init, cam_z_val):
     # maybe do error/type checking on the inputs later...
     curr_angle = 0
     # default_cc = 2  # can take this as an argument if we want ||| removed for now
     pos_array = []
     while curr_angle < 360:
-        pos_array.append(Coordinate_Generator.coords(x_init, curr_angle, 100))
+        pos_array.append(Coordinate_Generator.coords(x_init, curr_angle, cam_z_val))
         curr_angle += angle_step
     return pos_array
 

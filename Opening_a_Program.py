b47e4f5661e7dce4efaf31f13280c937dafc1048
cloftus96 <46966886+cloftus96@users.noreply.github.com>
2019-05-07 11:02:45 -0400

    Delete Opening_a_Program.py
    Unused file

--- Opening_a_Program.py
+++
@@ -1,8 +0,0 @@
-# This script opens Chrome in Windows
-# Change the path according to where your Chrome.exe file is located on your PC
-
-# class required for the necessary function
-import subprocess
-
-# This function opens the program!
-subprocess.Popen(['C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'])
